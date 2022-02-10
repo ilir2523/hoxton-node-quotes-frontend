b6a61af0cd3514fdd4cd6946f37c7e1f80063b8e
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-02-10 15:24:39 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>
+        "Make memes, not war!"
+        <strong> - Mark Twain</strong>
+      </p>
+      <p>
+        "Stop making quotes I never said!"
+        <strong> - Albert Einstein</strong>-
+      </p>
+    </div>
+  )
+}
+
+export default App
